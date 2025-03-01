73e591efa8212c729fa0b01e7f263774e3ae4d2e
SoYoung Lee <lsy000626@gmail.com>
2025-03-02 05:26:31 +0900

    css 요소 추가 적용 headline 클릭 이벤트 추가

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ const getNews = async () => {
 };
 
 const getLatestNews = async () => {
+  page = 1;
   url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
   );
